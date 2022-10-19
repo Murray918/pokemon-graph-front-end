bd928692e2cfec07a7d8d00c8dd619f5f88a101d
Murray918 <darbyguitars@gmail.com>
2022-10-18 20:20:10 -0500

    [andrew-#1] 	add about page 	import styled components 	add support for trailing commas

---
+++ pages/about/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    margin: 0 auto;
+    margin-left: 10vw;   
+`
+
+ export default Wrapper
